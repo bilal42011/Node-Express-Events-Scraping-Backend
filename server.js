ec6543d05280f84924b68953b5f49267425499fa
bilal42011 <87256843+bilal42011@users.noreply.github.com>
2022-06-07 17:19:51 +0500

    Updated dependecies

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import ifzrouter from "./routers/ifzrouter.js";
 import distilleryrouter from "./routers/distilleryrouter.js";
 import neueweltrouter from "./routers/neueweltrouter.js";
 
-const port = 9000;
+const port = 5000;
 app.use(cors());
 app.use("/mjut", mjutrouter);
 app.use("/ifz", ifzrouter);
